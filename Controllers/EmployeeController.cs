a7aad5164fcef95dff22e22f5014307f728b0132
Umutnaz <umutnaz@outlook.dk>
2025-11-07 10:41:57 +0100

    modul6 opgave 01 done

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -12,43 +12,96 @@ namespace IBASEmployeeService.Controllers
         {
             _logger = logger;
         }
-
-
         [HttpGet("GetEmployees")]
-        public IEnumerable<Employee> Get()
-        {
-            var employees = new List<Employee>() {
-            new Employee() {
-                Id = "21",
-                Name = "Mette Bangsbo",
-                Email = "meba@ibas.dk",
-                Department = new Department() {
-                    Id = 1,
-                    Name = "Salg"
-                }
-            },
-            new Employee() {
-                Id = "22",
-                Name = "Hans Merkel",
-                Email = "hame@ibas.dk",
-                Department = new Department() {
-                    Id = 2,
-                    Name = "Support"
-                }
-            },
-            new Employee() {
-                Id = "23",
-                Name = "Karsten Mikkelsen",
-                Email = "kami@ibas.dk",
-                Department = new Department() {
-                    Id = 2,
-                    Name = "Support"
-                }
+      public IEnumerable<Employee> Get()
+{
+    var employees = new List<Employee>() {
+        new Employee() {
+            Id = "21",
+            Name = "Mette Bangsbo",
+            Email = "meba@ibas.dk",
+            Department = new Department() {
+                Id = 1,
+                Name = "Salg"
             }
-        };
-            return employees;
+        },
+        new Employee() {
+            Id = "22",
+            Name = "Hans Merkel",
+            Email = "hame@ibas.dk",
+            Department = new Department() {
+                Id = 2,
+                Name = "Support"
+            }
+        },
+        new Employee() {
+            Id = "23",
+            Name = "Karsten Mikkelsen",
+            Email = "kami@ibas.dk",
+            Department = new Department() {
+                Id = 2,
+                Name = "Support"
+            }
+        },
+        // IT-afdelingen
+        new Employee() {
+            Id = "24",
+            Name = "Jonas Andersen",
+            Email = "joan@ibas.dk",
+            Department = new Department() {
+                Id = 3,
+                Name = "IT"
+            }
+        },
+        new Employee() {
+            Id = "25",
+            Name = "Sofie Holm",
+            Email = "soho@ibas.dk",
+            Department = new Department() {
+                Id = 3,
+                Name = "IT"
+            }
+        },
+        new Employee() {
+            Id = "26",
+            Name = "Martin Kjær",
+            Email = "makj@ibas.dk",
+            Department = new Department() {
+                Id = 3,
+                Name = "IT"
+            }
+        },
+        // Kantinen
+        new Employee() {
+            Id = "27",
+            Name = "Lone Rasmussen",
+            Email = "lora@ibas.dk",
+            Department = new Department() {
+                Id = 4,
+                Name = "Kantinen"
+            }
+        },
+        new Employee() {
+            Id = "28",
+            Name = "Peter Madsen",
+            Email = "pema@ibas.dk",
+            Department = new Department() {
+                Id = 4,
+                Name = "Kantinen"
+            }
         }
+    };
+
+    return employees;
+}
+        [HttpGet("GetByDepartment/{departmentName}")]
+        public IEnumerable<Employee> GetByDepartment(string departmentName)
+        {
+            var allEmployees = Get();
+            var result = allEmployees
+                .Where(e => e.Department.Name.Equals(departmentName, StringComparison.OrdinalIgnoreCase));
+            return result;
+        }
+        //how to i postman: http://localhost:5194/api/Employee/GetByDepartment/IT
     }
-
-
 }
